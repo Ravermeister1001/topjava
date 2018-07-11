e5b4cc60acd7b861db5d5532f115bfb429a5843a
Ravermeister <bgygblbketbl@mail.ru>
2018-07-11 18:12:44 +0300

    HW02 In memory meal repository, app layers for meal, including meal classes into spring application context.

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Collection;
 import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.*;
 
 @Controller
 public class MealRestController {
@@ -20,33 +23,38 @@ public class MealRestController {
     @Autowired
     private MealService service;
 
-    public Meal get(int id, int userId) {
-        log.info("get by id {} with user id {}", id, userId);
-        return service.get(id, userId);
+    public Meal get(int id) {
+        log.info("get by id {}", id);
+        Meal meal = service.get(id, SecurityUtil.authUserId());
+        validateUserWithException(meal.getUserId());
+        return meal;
     }
 
-    public Meal create(Meal meal, int userId) {
-        log.info("create meal {} with user id {}", meal, userId);
-        return service.create(meal, userId);
+    public Meal create(Meal meal) {
+        log.info("create meal {}", meal);
+        return service.create(meal, SecurityUtil.authUserId());
     }
 
-    public void delete(int id, int userId) {
-        log.info("delete by id {} with user id {}", id, userId);
-        service.delete(id, userId);
+    public void delete(int id) {
+        log.info("delete by id {}", id);
+        validateUserWithException(service.get(id, SecurityUtil.authUserId()).getUserId());
+        service.delete(id, SecurityUtil.authUserId());
     }
 
-    public void update(Meal meal, int userId) {
-        log.info("update {} with user id {}", meal, userId);
-        service.update(meal, userId);
+    public void update(Meal meal, int id) {
+        log.info("update {}", meal);
+        assureIdConsistent(meal, id);
+        validateUserWithException(meal.getUserId());
+        service.update(meal, SecurityUtil.authUserId());
     }
 
-    public Collection<MealWithExceed> getAll(int userId) {
-        log.info("getAll with user id {}", userId);
-        return service.getAll(userId);
+    public Collection<MealWithExceed> getAll() {
+        log.info("getAll");
+        return service.getAll(SecurityUtil.authUserId());
     }
 
-    public List<MealWithExceed> getBetween(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime, int userId) {
-        log.info("getAll between {} - {} and {} - {} with user id {} ", startDate, endDate, startTime, endTime, userId);
-        return service.getBetween(startDate, endDate, startTime, endTime, userId);
+    public List<MealWithExceed> getBetween(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+        log.info("getAll between {} - {} and {} - {}", startDate, endDate, startTime, endTime);
+        return service.getBetween(startDate, endDate, startTime, endTime, SecurityUtil.authUserId());
     }
 }
